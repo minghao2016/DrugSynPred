e41cf5f0825c921491e73fabe421104be0fd6374
Marzieh Ayati <marzieh@case.edu>
2015-12-01 21:14:57 -0500

    return drug by cell

--- code/propagate_Expression.m
+++ code/propagate_Expression.m
@@ -1,5 +1,5 @@
 
-function [D2D_prop_Exp,C2C_prop_Exp] = propagate_Expression(D2D, C2C , Expr_DS, alpha)
+function [Expr_DS_Prop_D2D,Expr_DS_Prop_C2C] = propagate_Expression(D2D, C2C , Expr_DS, alpha)
 
     
     [numberofDrug numberofCell] = size(Expr_DS);
@@ -9,23 +9,34 @@ function [D2D_prop_Exp,C2C_prop_Exp] = propagate_Expression(D2D, C2C , Expr_DS, 
     fprintf('Propagating expression through drug similarity network ...\n');
     %Propagate through D2D
     geneExpression = zeros(numberofGenes,numberofDrug);
+    Expr_DS_Prop_D2D=cell(numberofDrug, numberofCell);
     for i=1:numberofCell
         known = find(~cellfun(@isempty,Expr_DS(:,i)));
         if(known)
             geneExpression(:,known) = cell2mat(Expr_DS(known,i)');
             D2D_prop_Exp = propagation(D2D,alpha,geneExpression');
+            
+            for j=1:numberofDrug
+                Expr_DS_Prop_D2D{j,i} = num2cell(D2D_prop_Exp(j,:));
+            end
         end
+       
     end
     
     fprintf('Propagating expression through cell similarity network ...\n');
     %Propagate through C2C
     geneExpression = zeros(numberofGenes,numberofCell);
+    Expr_DS_Prop_C2C=cell(numberofDrug, numberofCell);
     for i=1:numberofDrug
         known = find(~cellfun(@isempty,Expr_DS(i,:)));
         if(known)
             geneExpression(:,known) = cell2mat(Expr_DS(i,known));
             C2C_prop_Exp = propagation(C2C,alpha,geneExpression');
+            for j=1:numberofCell
+                Expr_DS_Prop_C2C{i,j} = num2cell(C2C_prop_Exp(j,:));
+            end
         end
+         
     end
     
 end
