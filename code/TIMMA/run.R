4bb55ca3e3ca00926233c59d1288eabdd4e7eb3f
Shahin Mohammadi <mohammadi@purdue.edu>
2016-01-04 22:18:18 -0500

    first running TIMMA

--- code/TIMMA/run.R
+++ code/TIMMA/run.R
@@ -11,11 +11,12 @@ Dream_interaction_binary = read.table('../../Dream_interaction_binary.csv', sep=
 Interactions = Dream_interaction_binary[, 2:dim(Dream_interaction_binary)[2]];
 row.names(Interactions) = Dream_interaction_binary[, 1]
 
-
 Dream_sensitivity = read.table('../../Dream_sensitivity.csv', sep=',', header=T)
-Sensitivity = Dream_sensitivity[, 2:dim(Dream_interaction_binary)[2]];
+Sensitivity = Dream_sensitivity[, 2:dim(Dream_sensitivity)[2]];
 row.names(Sensitivity) = Dream_sensitivity[, 1]
 
 
 median_sensitivity<-Sensitivity[, 1]
-results<-timma(Interactions, median_sensitivity)
+results<-timma(as.matrix(Interactions), median_sensitivity)
+
+Syn = read.csv('predictedDrugScoring.csv')
